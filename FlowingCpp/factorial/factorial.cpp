a4033d104041a638eda47f462141abe2654685be
winterguard <aliasbonfire@gmail.com>
2019-04-17 22:56:59 +0900

    factorial

---
+++ FlowingCpp/factorial/factorial.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int factorial(int);
+
+int main() {
+	printf("%d", factorial(10));
+}
+
+int factorial(int number) {
+	int result = 1;
+	for (int i = 1; i <= number; i++) {
+		result *= i;
+	}
+	return result;
+}
