f7cb3bf7b3c05dc1b7462515a6b0ce3c909ebcab
winterguard <aliasbonfire@gmail.com>
2019-04-19 22:49:32 +0900

    add

--- FlowingCpp/GameOver/go.cpp
+++ FlowingCpp/GameOver/go.cpp
@@ -7,7 +7,7 @@ int main()
 	int kph = 100;
 	double mph;
 
-	printf("당신의 구속을 입력하시요[Km/H]: ");
+	printf("KM: ");
 	mph = kph / RateKphMph;
-	printf("당신의 구속은 %lf [MPH] 입니다.\n", mph);
+	printf(" %lf [MPH]\n", mph);
 }
